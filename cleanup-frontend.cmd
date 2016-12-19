a6802e1866b6a73b238060546e59aedb88b0aea3
Krzysztof Śmiałek <admin@avensome.net>
2016-12-19 16:43:40 +0100

    That's it

--- cleanup-frontend.cmd
+++
@@ -1,2 +0,0 @@
-docker stop host0/f0 host1/f1 host2/f2
-docker rm host0/f0 host1/f1 host2/f2
--- run-es.cmd
+++ run-es.cmd
@@ -3,10 +3,16 @@ docker-machine start host0 host1 host2
 timeout 2
 
 FOR /f "tokens=*" %%i IN ('docker-machine env --swarm host0') DO @%%i
+
+docker rm es0
 docker run -itd --name=es0 --network=iosrnet --network-alias=es --env="constraint:node==host0" es-multicast -Des.cluster.name=iosr -Des.discovery.zen.ping.multicast.enabled=true -Des.discovery.zen.ping.unicast.hosts="es" -Des.network.host=_eth0_
 timeout 3
+
+docker rm es1
 docker run -itd --name=es1 --network=iosrnet --network-alias=es --env="constraint:node==host1" es-multicast -Des.cluster.name=iosr -Des.discovery.zen.ping.multicast.enabled=true -Des.discovery.zen.ping.unicast.hosts="es" -Des.network.host=_eth0_
 timeout 3
+
+docker rm es2
 docker run -itd --name=es2 --network=iosrnet --network-alias=es --env="constraint:node==host2" es-multicast -Des.cluster.name=iosr -Des.discovery.zen.ping.multicast.enabled=true -Des.discovery.zen.ping.unicast.hosts="es" -Des.network.host=_eth0_
 timeout 10
 
